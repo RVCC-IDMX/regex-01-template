5ab514c1bb0a3d37029cfa1e9b1a7484530dfe41
cynthia <glean@cynthiateeters.com>
2021-09-27 17:52:16 -0400

    Add some challenges

--- tests/regex-01.test.js
+++ tests/regex-01.test.js
@@ -1,4 +1,11 @@
-const { testString, testStringMatch } = require('../src/regex-01');
+const {
+  testString,
+  testStringOr,
+  testStringIgnoreCase,
+  countVowels,
+  replaceVowels,
+  isPrefix,
+} = require('../src/regex-01');
 
 test('testString', () => {
   expect(testString('Hello, World!', /Hello/)).toBe(true);
@@ -6,16 +13,37 @@ test('testString', () => {
   expect(testString('Hello, World!', /Goodbye/)).toBe(false);
 });
 
-test('testStringMatch', () => {
+test('testStringOr', () => {
   const sampleArray = ['dog', 'cat', 'bird', 'fish'];
   let sampleString = 'Charlie has a pet dog named Snoopy.';
-  expect(testStringMatch(sampleString, sampleArray)).toBe(true);
+  expect(testStringOr(sampleString, sampleArray)).toBe(true);
   sampleString = 'John has a pet cat named Garfield.';
-  expect(testStringMatch(sampleString, sampleArray)).toBe(true);
+  expect(testStringOr(sampleString, sampleArray)).toBe(true);
   sampleString = 'Granny has a pet bird called Tweety.';
-  expect(testStringMatch(sampleString, sampleArray)).toBe(true);
+  expect(testStringOr(sampleString, sampleArray)).toBe(true);
   sampleString = 'Peppa has a pet fish named Goldie.';
-  expect(testStringMatch(sampleString, sampleArray)).toBe(true);
+  expect(testStringOr(sampleString, sampleArray)).toBe(true);
   sampleString = 'Daria has a pet rock named Igneous.';
-  expect(testStringMatch(sampleString, sampleArray)).toBe(false);
+  expect(testStringOr(sampleString, sampleArray)).toBe(false);
+});
+
+test('testStringIgnoreCase', () => {
+  expect(testStringIgnoreCase('Hello, World!', 'hello')).toBe(true);
+  expect(testStringIgnoreCase('Hello, World!', 'world')).toBe(true);
+  expect(testStringIgnoreCase('Hello, World!', 'goodbye')).toBe(false);
+});
+
+test('countVowels', () => {
+  expect(countVowels('Hello, World!')).toBe(3);
+  const sampleStr = `It matters not what someone is born, but what they grow to be.`;
+  expect(countVowels(sampleStr)).toBe(17);
+});
+
+test('replaceVowels', () => {
+  expect(replaceVowels('Hello, World!', '*')).toBe('H*llo, W*rld!');
+});
+
+test('isPrefix', () => {
+  expect(isPrefix('Hello, World!', 'Hello')).toBe(true);
+  expect(isPrefix('Hello, World!', 'World')).toBe(false);
 });
